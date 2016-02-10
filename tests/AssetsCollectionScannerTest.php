af4be9ab0d0b41989f6835e3a12cbb1eb8cdefca
Kessler <kessler.dev@gmail.com>
2016-02-10 09:56:57 +0100

    ajout de la methode invokeMethod dans les tests

--- tests/AssetsCollectionScannerTest.php
+++ tests/AssetsCollectionScannerTest.php
@@ -64,4 +64,21 @@ class AssetsCollectionScannerTest extends TestCase
         $this->assertContains('js-stack-dark-glow#css/jsstack.dark-glow.css',$jsStackCollection);
 
     }
+    
+     /**
+     * Call protected/private method of a class.
+     *
+     * @param object &$object    Instantiated object that we will run method on.
+     * @param string $methodName Method name to call
+     * @param array  $parameters Array of parameters to pass into method.
+     *
+     * @return mixed Method return.
+     */
+    public function invokeMethod(&$object, $methodName, array $parameters = array())
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+        return $method->invokeArgs($object, $parameters);
+    }
 }
