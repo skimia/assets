101ec1299b441f4f37cdbfa00f243e47163de0f5
Travis <travis@travis-ci.org>
2016-02-14 14:28:06 +0000

    API updated

---
+++ api/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Skimia\\Assets\\AssetsServiceProvider"],["c","Skimia\\Assets\\Console\\Commands\\DumpCollectionsCommand"],["c","Skimia\\Assets\\Console\\Commands\\FlushPipelineCommand"],["c","Skimia\\Assets\\Events\\BeforeMergeCollectionFiles"],["c","Skimia\\Assets\\Manager"],["c","Skimia\\Assets\\Providers\\StolzAssetsServiceProvider"],["c","Skimia\\Assets\\Scanner\\Scanner"],["c","Skimia\\Assets\\Scanner\\ScannerServiceProvider"]];
